a4cb6332810eed27d1fb597adcab0e3426398a07
ibai <achita145@gmail.com>
2019-02-12 14:22:20 +0100

    sql

--- src/modelo/dao/UsuarioModelo.java
+++ src/modelo/dao/UsuarioModelo.java
@@ -11,17 +11,21 @@ public class UsuarioModelo extends Conector{
 	
 	public void addUser(Usuario usuario) {
 		//Recive datos de la clase usuario y añade un nuevo usuario
+		//INSERT INTO usuarios(nombre, apellido, usuario, e-mail, contrasenia, contrasenia2, dni, fecha_nacimiento,direccion, provincia, ciudad, cp) 
+		//VALUES ("","","","","","","","","","","","")
 	}
 	
 	public void deleteUser(int id) {
 		//Recive el id y borra el usuario
+		//DELETE FROM usuarios WHERE id='';
 	}
 	
 	public void updateUser(Usuario usuario) {
-		//Recive datos de la clase usuario y lo   
+		//Recive datos de la clase usuario y los modifica  
 	}
 	
-	public void viewAll() {
+	public void selectAll() {
 		//Pasa la arraylist de usuarios mostrando todos los usuarios
+		//SELECT * FROM usuarios
 	}
 }
