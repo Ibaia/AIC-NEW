0c6367e475a53977ff96eab17467ff5b6d4aaf08
Carlos <isla.carlos.99@gmail.com>
2019-02-12 13:51:24 +0100

    implementar doGet

--- src/controlador/CrearUsuario.java
+++ src/controlador/CrearUsuario.java
@@ -27,7 +27,7 @@ public class CrearUsuario extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Registro.jsp").append(request.getContextPath());
 	}
 
 	/**
